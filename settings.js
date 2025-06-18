ae331d1c10f08d18ff9547abb266626ee9c2d2ad
afrochris <appiahchristian900@gmail.com>
2025-06-18 23:37:08 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Knight Bot', // Your name
-  ownerNumber: '14703105589', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Afro Chris', // Your name
+  ownerNumber: '0533545948', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
